ff9c0d556217c28c5ad47701aa22c7846dc0d706
Savita Katwe <savitamerwade04@gmail.com>
2024-10-16 21:55:22 +0400

    Added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Image from "next/image";
-import Logo from '../../public/svg/logo.svg'
+import Logo from "../../public/svg/logo.svg";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,9 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <div>
-        <Image alt={''} src={Logo}/>
-      </div>
+        <div className="h-20 flex flex-col justify-center px-5">
+          <Image alt={""} src={Logo} />
+        </div>
         {children}
       </body>
     </html>
