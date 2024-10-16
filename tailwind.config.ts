d41327b06383477648bf71b3511b08fa0cee31c4
Savita Katwe <savitamerwade04@gmail.com>
2024-10-16 20:39:06 +0400

    Added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,33 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      h1: ['44px', {
+        fontWeight: 600,
+        lineHeight: '48px',
+        letterSpacing: '-1.5%'
+      }],
+      h2: ['40px', {
+        fontWeight: 600,
+        lineHeight: '44px',
+        letterSpacing: '-1%'
+      }],
+      h3: ['32px', {
+        fontWeight: 600,
+        lineHeight: '36px',
+        letterSpacing: '0'
+      }],
+      h4: ['28px', {
+        fontWeight: 600,
+        lineHeight: '34px',
+        letterSpacing: '0'
+      }],
+      h5: ['24px', {
+        fontWeight: 500,
+        lineHeight: '30px',
+        letterSpacing: '0'
+      }]
+    },
     extend: {
       colors: {
         background: "var(--background)",
