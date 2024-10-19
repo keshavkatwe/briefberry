7fee7c3ecd4c568193cc3a5fff40d5b513e0edaf
Keshav Katwe <keshav.katwe@gmail.com>
2024-10-19 20:03:51 +0400

    Added input

--- src/widgets/SignInWidget/SignInWidget.tsx
+++ src/widgets/SignInWidget/SignInWidget.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Modal } from "@/components";
+import { Field, Input, Label } from "@headlessui/react";
 
 const SignInWidget = () => {
   return (
@@ -15,6 +16,18 @@ const SignInWidget = () => {
             Sign up with google
           </button>
           <p className={"text-small text-text-tertiary"}>Or use your email</p>
+          <div>
+            <Field>
+              <Label className="text-sm/6 font-medium text-white">
+                Street address
+              </Label>
+              <Input
+                className={
+                  "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+                }
+              />
+            </Field>
+          </div>
         </div>
       </Modal>
     </>
