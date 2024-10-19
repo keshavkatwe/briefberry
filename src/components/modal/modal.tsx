fe000ce8ca0b6f919e7b9b24967bef28025eb548
Savita Katwe <savitamerwade04@gmail.com>
2024-10-19 14:38:03 +0400

    Added to modal component

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,2 +1,35 @@
-const Modal = () => {};
+import {
+  Description,
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  DialogProps,
+} from "@headlessui/react";
+import { PropsWithChildren } from "react";
+
+interface IModalProps extends DialogProps {
+  title?: string;
+  description?: string;
+}
+const Modal = ({
+  children,
+  title,
+  description,
+}: PropsWithChildren<IModalProps>) => {
+  return (
+    <Dialog open={true} className="relative z-50" onClose={() => {}}>
+      <DialogBackdrop className="fixed inset-0 bg-background-overlay" />
+      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+        <DialogPanel className="max-w-lg space-y-4 bg-background-surface1 p-12 rounded-32">
+          {title && (
+            <DialogTitle className="text-h3 text-center">{title}</DialogTitle>
+          )}
+          {description && <Description>{description}</Description>}
+          {children}
+        </DialogPanel>
+      </div>
+    </Dialog>
+  );
+};
 export default Modal;
--- src/widgets/SignInWidget/SignInWidget.tsx
+++ src/widgets/SignInWidget/SignInWidget.tsx
@@ -1,32 +1,15 @@
 "use client";
-import {
-  Description,
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
+import { Modal } from "@/components";
 
 const SignInWidget = () => {
   return (
     <>
-      <Dialog open={true} className="relative z-50" onClose={() => {}}>
-        <DialogBackdrop className="fixed inset-0 bg-background-overlay" />
-        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-          <DialogPanel className="max-w-lg space-y-4 bg-background-surface1 p-12 rounded-32">
-            <DialogTitle className="text-h3 text-center">
-              Sign in to Briefberry
-            </DialogTitle>
-            <Description>
-              This will permanently deactivate your account
-            </Description>
-            <p>
-              Are you sure you want to deactivate your account? All of your data
-              will be permanently removed.
-            </p>
-          </DialogPanel>
-        </div>
-      </Dialog>
+      <Modal
+        open={true}
+        className="relative z-50"
+        onClose={() => {}}
+        title={"Sign in to Briefberry"}
+      ></Modal>
     </>
   );
 };
